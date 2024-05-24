9c7401bab2eaa7b973706b5cf57a1f1d961897a7
adrian <adrian.grillet@epita.fr>
2024-05-24 17:34:36 +0200

    feat: step 2 (not tested)

--- instruction_control_unit/instruction_control_unit.vhd
+++ instruction_control_unit/instruction_control_unit.vhd
@@ -6,9 +6,10 @@ entity instruction_control_unit is
     port (
         Offset : in std_logic_vector(23 downto 0);
         nPCsel : in std_logic;
-        Instruction : out std_logic_vector(31 downto 0)
+        Instruction : out std_logic_vector(31 downto 0);
+        Clk, Reset : in std_logic
     );
-end entity
+end instruction_control_unit;
 
 architecture arch of instruction_control_unit is
     signal PC : std_logic_vector(31 downto 0);
@@ -16,6 +17,11 @@ architecture arch of instruction_control_unit is
     signal selected : std_logic_vector(31 downto 0);
 begin
     -- make PC_unit with clock and input output
+    PC : entity work.pc_unit port map(PC_in => selected, PC_out => PC, Clk => Clk, Reset => Reset);
     -- make PCSel_unit with input(PC, Offset_Ext) output(selected)
-
+    PC_selection : entity work.pc_selection_unit port map(Offset => Offset_ext, PC => PC,
+                PC_selected => selected, nPCsel => nPCsel);
+    SignExt : entity work.EXTENDER generic map(N => 24) 
+                port map(DataIn => Offset, DataOut => Offset_ext);
+    Instr_mem : entity work.instruction_memory port map(PC => PC, Instruction => Instruction);
 end arch ; -- arch
